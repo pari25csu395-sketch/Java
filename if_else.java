d330d5c130052a6823c973fdffd3e6ea6dbb316a
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-07-31 00:40:21 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class if_else {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("AN ADULT");
+        }
+        else{
+            System.out.println("Not adult");
+
+        }
+
+    }
+    
+}

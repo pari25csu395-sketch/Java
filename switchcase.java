bd69621bde3abe8905b8513cc8a1241e8221a492
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-07-31 00:39:31 +0530

    Add files via upload

---
+++ switchcase.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class switchcase {
+    public static void main(String args[]){
+        Scanner sc = new Scanner((System.in));
+        int button = sc.nextInt();
+
+        switch(button){
+            case 1 : System.out.println("NAMASTE");
+            break;
+            case 2 : System.out.println("HELLO");
+            break;
+            case 3 : System.out.println("BONJOUR");
+            break;
+            default:
+                System.out.println("INVALID");
+
+        }
+
+
+    }
+    
+    
+}

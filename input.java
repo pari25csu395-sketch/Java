2d6e373e0c6320749f2c85df0d734061e00f4daa
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-07-31 00:40:03 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class input {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine(); //nextlINE IS FOR MANY LINES
+        System.out.println(name);
+
+        
+    }
+    
+    
+}

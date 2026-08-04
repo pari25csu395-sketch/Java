5d6c1a5a25a3bb7bc2e597de1fc4b1d8395e7836
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-08-04 12:43:02 +0530

    Add files via upload

---
+++ class1.java
@@ -0,0 +1,18 @@
+// # WAP TO PRINT EVERY CHARACTER OF STRING
+
+import java.util.*;
+public class class1 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for(int i = 0; i<=str.length(); i++){
+            System.out.println(str.charAt(i));
+        }
+        
+
+
+
+        
+    }
+    
+}

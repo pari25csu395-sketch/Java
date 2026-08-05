624ed209451d289223bb73d22f4395c885316fd6
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-08-05 23:48:57 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Arrays {
+    public static void main(String args[]){
+        int [] marks = new int[3];
+        marks[0] = 98;
+        marks[1] = 76;
+        marks[2] = 8
+    }
+    
+}

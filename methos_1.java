d64a4d1363d72dbb24909526e6290545a417066f
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-08-11 23:07:57 +0530

    Add files via upload

---
+++ methos_1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class methos_1 {
+    static void print2katable(){
+        for(int i = 1 ;i<=10; i++){
+            System.out.println("->" + 2*i);
+        
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("hi");
+        print2katable();
+        System.out.println("byie");
+        
+    }
+    
+}

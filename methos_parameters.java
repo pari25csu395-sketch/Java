92c8ccc535367053912d7bc36e88c8df4942242b
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-08-11 23:08:13 +0530

    Add files via upload

---
+++ methos_parameters.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class methos_parameters {
+    static int add(int a , int b){
+        return a+b;
+    }
+    
+
+public static void main(String[] args){
+    int result = add(8,9);
+    System.out.println(result);
+
+    }
+}

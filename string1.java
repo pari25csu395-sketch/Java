728135ad6e27d6b57c3340b66728a063081c84a9
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-08-20 00:18:31 +0530

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,11 @@
+public class string1 {
+    public static void main(String[] args) {
+        String firstname = "Pari";
+        String lastname = "Aggarwal";
+        System.out.println(firstname + " " + lastname);
+
+        System.out.println( firstname.length());
+        System.out.println(firstname.charAt(0));
+    }
+    
+}

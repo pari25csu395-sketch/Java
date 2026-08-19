ed8b9fa75b7667445aaa4cb85c58b5d1aaada444
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-08-20 00:49:35 +0530

    Add files via upload

--- q2.java
+++ q2.java
@@ -1,30 +1,20 @@
-// wap to find the max value of element inside the array
-import java.util.*;
+// count the vowela in a string
 
 public class q2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int array[] = new int[size];
+        String str = " i lOve my family";
+        int count = 0;
+        for( int i = 0; i< str.length(); i++){
 
-        for ( int i = 0; i<size; i++){
-            System.out.println("provide element for index " +i);
-            array[i] = sc.nextInt();
-        }
-
-        int maxvalue = array[0];
-
-        for ( int i = 0; i<size; i++){ // comparing values
-            if(array[i] > maxvalue){ 
-                maxvalue = array[i]; // update
-
+            char ch = str.charAt(i);
+            if(ch == 'a'|| ch == 'e'|| ch == 'i'|| ch == 'o' || ch == 'u'|| ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+                count = count+1;
             }
-           
         }
-        System.out.println("max value is " + maxvalue);
+        System.out.println(count);
+        
 
-
+        
     }
-
     
 }

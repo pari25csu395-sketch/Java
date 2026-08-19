ffe5e0083d8145201db652623bcdde688e82ef75
pari25csu395-sketch <pari25csu395@ncuindia.edu>
2026-08-20 00:49:18 +0530

    Add files via upload

--- q1.java
+++ q1.java
@@ -1,31 +1,11 @@
-import java.util.Scanner;
+// print each character of a string.
 
 public class q1 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int size = sc.nextInt();
-        int array[] = new int[size];
-
-        for(int i = 0; i<size; i++){
-            System.out.println("provide element for index 2" +i);
-            array[i] = sc.nextInt();
-
+        String str = " Pari";
+        for ( int i  = 0; i < str.length(); i++){
+            System.out.println(str.charAt(i));
         }
-        int sum = 0;
-
-        for(int i = 0; i<size; i++){
-           sum += array[i];
-            System.out.println(sum);
-
-
-        }
-
-
-
-
-
-
-        
     }
     
 }
